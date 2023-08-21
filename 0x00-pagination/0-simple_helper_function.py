075b99a3a9aea6da33cebee25951f0694650298d
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-21 12:55:15 +0300

    mule

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+
 """
 Module 0-simple_helper_function
 """
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    index_tuple = page_size * (page - 1), page * page_size
-    return index_tuple
+def index_range(page, page_size):
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
