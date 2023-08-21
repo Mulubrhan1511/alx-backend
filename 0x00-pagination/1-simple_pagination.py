3d4e3929d634e110aed867b9853b256c475c6d32
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-21 13:23:55 +0300

    mule

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module 1-simple_pagination
+"""
 import csv
 import math
 from typing import List
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """
     Takes two integer arguments page and page_size.
     Returns a tuple of size two containing a start
@@ -11,9 +16,8 @@ def index_range(page, page_size):
     of indexes to return in a list for those particular
     pagination parameters.
     """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+    index_tuple = page_size * (page - 1), page * page_size
+    return index_tuple
 
 
 class Server:
@@ -22,6 +26,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """init object"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -32,19 +37,18 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
-            paginate dataset
-            """
-            assert type(page) is int and page > 0
-            assert type(page_size) is int and page_size > 0
+        """
+        paginate dataset
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
-            indexes = index_range(page, page_size)
-            try:
-                data = self.dataset()
-                return data[indexes[0]: indexes[1]]
-            except IndexError:
-                return []
+        indexes = index_range(page, page_size)
+        try:
+            data = self.dataset()
+            return data[indexes[0]: indexes[1]]
+        except IndexError:
+            return []
