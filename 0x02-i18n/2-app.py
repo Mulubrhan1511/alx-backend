b4e8169cd359aa79ff87e4ad527c5dfac64ac398
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-29 12:21:23 +0300

    mule

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+file: 1-app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """configration file"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """display the hello world message"""
+    return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """gets best languages"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
